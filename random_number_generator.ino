aa0d9563a35afb311d15b14ac79387d718d51c8c
animeshjavali <33710255+animeshjavali@users.noreply.github.com>
2017-11-15 20:58:46 -0800

    Update random_number_generator.ino

--- random_number_generator.ino
+++ random_number_generator.ino
@@ -5,9 +5,9 @@ void setup() {
 
 
 void loop() {
-Serial.print(F("{\"uid\": \"EC1234\",\"type\": \"humidty\",\"value\":"));
+Serial.print(F("{\"uid\": \"EC1234\",\"type\": \"humidty\",\"value\":"));// JSON into flash memeory.
 
-Serial.print( get_rand(100));
+Serial.print( get_rand(100));// call the user defined function.
 Serial.println("}");
 delay(1000);
 
@@ -17,5 +17,5 @@ delay(1000);
 int get_rand(int val)
 {
   int seed=analogRead(A0);
-  return ((seed%22)*999)%val;
+  return ((seed%22)*999)%val;// Generate a random number.
   }
