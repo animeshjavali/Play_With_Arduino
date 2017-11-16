72f0726b81665cc07dd26e957f6f12e7590702fa
animeshjavali <33710255+animeshjavali@users.noreply.github.com>
2017-11-15 20:54:42 -0800

    Update gsm_call.ino

--- gsm_call.ino
+++ gsm_call.ino
@@ -5,14 +5,14 @@ SoftwareSerial mySerial(5,6);//RX,TX
 
 void setup() {
 Serial.begin(9600);
-mySerial.begin(9600);
+mySerial.begin(9600);// Start GSM serial communication.
 delay(2000);
 }
 
 
 void loop() {
-  mySerial.println("ATD9482199632;");
-  Serial.write("ATD9482199632;");
+  mySerial.println("ATD9482199632;");// Generate a call using AT command.
+  Serial.write("ATD9482199632;");//Print it on the serial monitor.
   while(1);
   
 
