116ebec19e58a74d3a8bd630d5324c6e5975b981
animeshjavali <33710255+animeshjavali@users.noreply.github.com>
2017-11-15 20:37:28 -0800

    Update Intruder.ino

--- Intruder.ino
+++ Intruder.ino
@@ -1,19 +1,19 @@
 int readval;
 void setup() {
- Serial.begin(9600);
- pinMode(8,OUTPUT);
+ Serial.begin(9600);// Start serial communication.
+ pinMode(8,OUTPUT);// Define pin Modes.
  pinMode(7,INPUT);
 
 }
 
 void loop() {
   digitalWrite(7,LOW);
-  readval=digitalRead(7);
+  readval=digitalRead(7);// Set readval to 1 if there's an intruder. Otherwise 0.
   Serial.println(readval);
   delay(500);
   if(readval!=0)
   {
-  digitalWrite(8,HIGH);
+  digitalWrite(8,HIGH);// Drive an indicator LED or Buzzer.
   }
   else
     digitalWrite(8,LOW);
