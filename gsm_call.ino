a887c54c4c698c47fb2d070919f8757fd52949a8
animeshjavali <33710255+animeshjavali@users.noreply.github.com>
2017-11-15 20:42:48 -0800

    Add files via upload

---
+++ gsm_call.ino
@@ -0,0 +1,21 @@
+#include <SoftwareSerial.h>
+SoftwareSerial mySerial(5,6);//RX,TX
+
+
+
+void setup() {
+Serial.begin(9600);
+mySerial.begin(9600);
+delay(2000);
+}
+
+
+void loop() {
+  mySerial.println("ATD9482199632;");
+  Serial.write("ATD9482199632;");
+  while(1);
+  
+
+  
+ 
+}
---
+++ random_number_generator.ino
@@ -0,0 +1,21 @@
+void setup() {
+  Serial.begin(9600);
+
+}
+
+
+void loop() {
+Serial.print(F("{\"uid\": \"EC1234\",\"type\": \"humidty\",\"value\":"));
+
+Serial.print( get_rand(100));
+Serial.println("}");
+delay(1000);
+
+
+
+}
+int get_rand(int val)
+{
+  int seed=analogRead(A0);
+  return ((seed%22)*999)%val;
+  }
