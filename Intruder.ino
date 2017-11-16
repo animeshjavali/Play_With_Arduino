0a9d26cf2b3f997e565415553a5b2acbe577a332
animeshjavali <33710255+animeshjavali@users.noreply.github.com>
2017-11-15 20:32:14 -0800

    Add files via upload

---
+++ Intruder.ino
@@ -0,0 +1,20 @@
+int readval;
+void setup() {
+ Serial.begin(9600);
+ pinMode(8,OUTPUT);
+ pinMode(7,INPUT);
+
+}
+
+void loop() {
+  digitalWrite(7,LOW);
+  readval=digitalRead(7);
+  Serial.println(readval);
+  delay(500);
+  if(readval!=0)
+  {
+  digitalWrite(8,HIGH);
+  }
+  else
+    digitalWrite(8,LOW);
+}
