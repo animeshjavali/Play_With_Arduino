4688710921eb4e7c2c2c3da1d56c3ee72c4cb8c9
Animesh Javali <33710255+animeshjavali@users.noreply.github.com>
2018-03-04 12:33:42 +0530

    Add files via upload

---
+++ Pulse_Width_Modulation.ino
@@ -0,0 +1,20 @@
+int writevalue,readvalue;
+float readvoltage,writevoltage;
+
+
+void setup() {
+  // put your setup code here, to run once:
+pinMode(A0,INPUT);
+pinMode(9,OUTPUT);
+Serial.begin(9600);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  readvalue=analogRead(A0);
+  readvoltage=(5./1023.)*readvalue;
+  writevalue=(255./1023.)*readvalue;
+  writevoltage=(5.*20/255.)*writevalue;
+  delay(1000);
+  Serial.println(writevoltage);
+}
